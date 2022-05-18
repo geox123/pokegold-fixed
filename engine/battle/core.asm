6626c62e2a5b2de44e800f5b9ece34d6bbee9055
Spencer Churchill <spence@duck.com>
2022-05-18 00:33:36 -0400

    pp up undefined behavior

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5295,8 +5295,7 @@ MoveSelectionScreen:
 	jr .loop
 
 .done
-	; Bug: this will result in a move with PP Up confusing the game.
-	and a ; should be "and PP_MASK"
+	and PP_MASK
 	ret nz
 
 .force_struggle
